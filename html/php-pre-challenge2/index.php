1c9dbffbd803ad30a6a6d38d7fd4868cdb17ba01
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-08-18 09:36:32 +0900

    first commit

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -3,10 +3,16 @@ $array = explode(',', $_GET['array']);
 
 // 修正はここから
 for ($i = 0; $i < count($array); $i++) {
-
+        if($array[$i-1]>$array[$i]){
+            $baseValue=$array[$i-1];
+            $array[$i-1]=$array[$i];
+            $array[$i]=$baseValue;            
+        }    
 }
 // 修正はここまで
 
 echo "<pre>";
 print_r($array);
 echo "</pre>";
+
+?>
