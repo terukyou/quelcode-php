d42d2bbbe267531c1189cb918438dac4edd4e7e4
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-08-18 15:59:13 +0900

    for文の追加

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -2,12 +2,16 @@
 $array = explode(',', $_GET['array']);
 
 // 修正はここから
-for ($i = 0; $i < count($array); $i++) {
-        if($array[$i-1]>$array[$i]){
-            $baseValue=$array[$i-1];
-            $array[$i-1]=$array[$i];
-            $array[$i]=$baseValue;            
-        }    
+$number = count($array);
+
+for ($i = 0; $i < $number; $i++) {
+    for($j = 0; $j < $number-$i; $j++){
+        if($array[$j-1]>$array[$j]){
+            $baseValue=$array[$j-1];
+            $array[$j-1]=$array[$j];
+            $array[$j]=$baseValue;            
+        }
+    }
 }
 // 修正はここまで
 
@@ -15,4 +19,3 @@ echo "<pre>";
 print_r($array);
 echo "</pre>";
 
-?>
