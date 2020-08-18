e0c9d7055d5606422798523b8d47d1bce02eed27
terukyou <z18e04n3@tbox.nara-u.ac.jp>
2020-08-18 16:11:46 +0900

    コード整形

--- html/php-pre-challenge2/index.php
+++ html/php-pre-challenge2/index.php
@@ -5,11 +5,11 @@ $array = explode(',', $_GET['array']);
 $number = count($array);
 
 for ($i = 0; $i < $number; $i++) {
-    for($j = 0; $j < $number-$i; $j++){
-        if($array[$j-1]>$array[$j]){
-            $baseValue=$array[$j-1];
-            $array[$j-1]=$array[$j];
-            $array[$j]=$baseValue;            
+    for($j = 0; $j < $number - $i; $j++){
+        if($array[$j-1] > $array[$j]){
+            $baseValue = $array[$j-1];
+            $array[$j-1] = $array[$j];
+            $array[$j] = $baseValue;            
         }
     }
 }
